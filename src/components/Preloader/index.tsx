8941efe3d82b28e593fef7440d07fb96a53e6c4e
saur0w <sthapliyal085@gmail.com>
2026-08-16 16:18:19 +0530

    feat: initialize project structure with global styles and Preloader component

---
+++ src/components/Preloader/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./style.module.scss";
+
+export default function Preloader() {
+    return (
+        <section className={styles.preloader}>
+
+        </section>
+    );
+}
