b1d2479553429807ac4080a6d0c88f126ba7b1b8
saur0w <sthapliyal085@gmail.com>
2026-08-17 09:43:32 +0530

    feat: initialize homepage layout with Header and Landing components

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -5,7 +5,9 @@ import styles from "./style.module.scss";
 export default function Landing() {
     return (
         <section className={styles.landing}>
+            <div className={styles.body}>
 
+            </div>
         </section>
     );
 }
---
+++ src/ui/PerspectiveText/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./style.module.scss";
+
+export default function PerspectiveText() {
+    return (
+        <section className={styles.content}>
+
+        </section>
+    );
+}
