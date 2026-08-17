6e72c451eb65ecbe35369986d5090209b8a5372e
saur0w <sthapliyal085@gmail.com>
2026-08-17 09:18:17 +0530

    "Landing Structure"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import styles from "./page.module.css";
 import Preloader from "@/components/Preloader";
+import Landing from "@/components/Landing";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Preloader />
+        <Landing />
     </div>
   );
 }
